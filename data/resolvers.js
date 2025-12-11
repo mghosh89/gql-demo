3c30107acfea935aa706b667b31bf5d375bc63a3
Bulu <bulu@Bulus-MacBook-Pro.local>
2025-12-11 08:26:51 -0500

    create story and save to db from user input

--- data/resolvers.js
+++ data/resolvers.js
@@ -10,14 +10,32 @@ const resolvers = {
     },
     // fetch all stories
     stories: async () => {
-      return await Stories.find();
+      const allstories = await Stories.find();
+      return allstories;
     },
   },
+ /*//This mutations part is added to create new story 
   Mutation: {
     // create a new story document
     createStoryMut: async (_, { input }) => {
       const created = await Stories.create(input);
       return created;
+    },
+  },*/
+  
+  // Mutation to create a story in the database
+  Mutation: {
+    createStoryMut: async (_, { input }) => {
+        const newStory = new Stories({
+            name: input.name,
+            description: input.description,
+            votedStatus: input.votedStatus,
+            metadata: input.metadata,
+            ageRating: input.ageRating,
+        });
+        newStory.id = newStory._id; // Assign the generated _id to id field
+        await newStory.save();
+        return newStory;
     },
   },
 };
